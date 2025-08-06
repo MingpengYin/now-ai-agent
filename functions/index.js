12cdc9752ce5d3e5c407f4e5b63e8455c070fba1
MingpengYin <imp3orc@gmail.com>
2025-08-06 10:00:07 +0800

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,4 @@
-export async function onRequest(context) {
+export default async function onRequest(context) {
   // 从环境变量中读取 DISABLE_PAGE 的值
   const isPageDisabled = context.env.DISABLE_PAGE === 'true';
 
@@ -9,6 +9,5 @@ export async function onRequest(context) {
   }
 
   // 如果页面没有被禁用，则正常处理请求，返回 index.html
-  // context.next() 会执行正常的 Pages 路由，返回 index.html
   return context.next();
 }
